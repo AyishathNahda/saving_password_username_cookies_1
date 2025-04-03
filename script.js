838264f25236e421a3230d23c40da9adf43b485d
kalvium <kalvium@kalvium.community>
2025-04-03 11:08:44 +0530

    done

--- script.js
+++ script.js
@@ -15,9 +15,18 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
+  let count=getCookie('count');
+
   // 2. If the cookie exists, increment the value and update the cookie
+  if(count!==null){
+    count++;
+  }else{
+    count=1;
+  }
+  setCookie('count',count,7);
   // 3. If the cookie does not exist, create it and set the value to 1
   // 4. Display the count on the webpage
+  document.body.innerHTML=`<h1>Page Load Count: ${count}</h1>`;
 
   // your code here
 });
